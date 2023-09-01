e6483f3d5c358875fc06202a707066658e1ac3a7
m1ngoDo <vsm0803@gmail.com>
2023-09-01 14:20:45 +0900

    reactive-streams

---
+++ practices/reactive-streams/src/main/java/com/reactivestreams/SimpleHotPublisherMain.java
@@ -0,0 +1,52 @@
+package com.reactivestreams;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * packageName    : com.reactivestreams
+ * fileName       : SimpleHotPublisherMain
+ * author         : okdori
+ * date           : 2023/09/01
+ * description    :
+ */
+@Slf4j
+public class SimpleHotPublisherMain {
+    @SneakyThrows
+    public static void main(String[] args) {
+        // prepare publisher
+        var publisher = new SimpleHotPublisher();
+
+        // prepare subscriber1
+        var subscriber = new SimpleNamedSubscriber<>("subscriber1");
+        publisher.subscribe(subscriber);
+
+        // cancel after 5s
+        Thread.sleep(5000);
+        subscriber.cancel();
+
+        // prepare subscriber2,3
+        var subscriber2 = new SimpleNamedSubscriber<>("subscriber2");
+        var subscriber3 = new SimpleNamedSubscriber<>("subscriber3");
+        publisher.subscribe(subscriber2);
+        publisher.subscribe(subscriber3);
+
+        // cancel after 5s
+        Thread.sleep(5000);
+        subscriber2.cancel();
+        subscriber3.cancel();
+
+
+        Thread.sleep(1000);
+
+        var subscriber4 = new SimpleNamedSubscriber<>("subscriber4");
+        publisher.subscribe(subscriber4);
+
+        // cancel after 5s
+        Thread.sleep(5000);
+        subscriber4.cancel();
+
+        // shutdown publisher
+        publisher.shutdown();
+    }
+}
---
+++ practices/reactive-streams/src/main/java/com/reactivestreams/SimpleNamedSubscriber.java
@@ -0,0 +1,50 @@
+package com.reactivestreams;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.Flow;
+
+/**
+ * packageName    : com.reactivestreams
+ * fileName       : SimpleNamedSubscriber
+ * author         : okdori
+ * date           : 2023/09/01
+ * description    :
+ */
+@Slf4j
+public class SimpleNamedSubscriber<T> implements Flow.Subscriber<T> {
+    private Flow.Subscription subscription;
+    private final String name;
+
+    public SimpleNamedSubscriber(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void onSubscribe(Flow.Subscription subscription) {
+        this.subscription = subscription;
+        this.subscription.request(1);
+        log.info("onSubscribe");
+    }
+
+    @Override
+    public void onNext(T item) {
+        log.info("name: {}, onNext: {}", name, item);
+        this.subscription.request(1);
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+        log.error("onError: {}", throwable.getMessage());
+    }
+
+    @Override
+    public void onComplete() {
+        log.info("onComplete");
+    }
+
+    public void cancel() {
+        log.info("cancel");
+        this.subscription.cancel();
+    }
+}
